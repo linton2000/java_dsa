7ed540f4a35f3d1c31cb2d9ac4f7a70cc12747f6
Linton Charles <lintoncharles2000@gmail.com>
2025-05-25 14:48:24 +1000

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
