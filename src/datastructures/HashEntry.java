1c647980ea7c9b44f4f93882abac9e127380e005
Linton Charles <lintoncharles2000@gmail.com>
2025-06-02 10:34:43 +1000

    Make HashEntry attributes public

--- src/datastructures/HashEntry.java
+++ src/datastructures/HashEntry.java
@@ -1,8 +1,8 @@
 package datastructures;
 
 public class HashEntry {
-    String key;
-    String val;
+    public String key;
+    public String val;
 
     public HashEntry(String key, String val){
         this.key = key;
