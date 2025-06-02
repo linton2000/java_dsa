25edf2843567ad34d835943d8f6d56ef01759526
Linton Charles <lintoncharles2000@gmail.com>
2025-06-02 15:27:19 +1000

    Implement collision handling through chaining

--- src/datastructures/HashEntry.java
+++ src/datastructures/HashEntry.java
@@ -3,10 +3,15 @@ package datastructures;
 public class HashEntry {
     public String key;
     public String val;
+    public HashEntry chainedEntry;
 
     public HashEntry(String key, String val){
         this.key = key;
         this.val = val;
+    }
+
+    public void chainEntry(HashEntry nextEntry){
+        this.chainedEntry = nextEntry;
     }
 
     @Override
--- src/datastructures/HashMap.java
+++ src/datastructures/HashMap.java
@@ -1,11 +1,20 @@
 package datastructures;
 
+/* 
+ * Simple HashMap implementation that uses Chaining to resolve collisions.
+ * Hashes based on ASCII sum of key (modded by capacity).
+ */
 public class HashMap {
     HashEntry[] array;
     int length;  // No. of elements in HashMap
     int capacity;  // Size of backing array
-    int verbosity; // 0 - No prints; 1 - All prints
+    int verbosity;
 
+    /**
+     * Base constructor
+     * 
+     * @param verbosity For debug statements: 0 - No prints; 1 - All prints
+     */
     public HashMap(int verbosity){
         this.length = 0;
         this.capacity = 2;  // Arbitrary initial capacity
@@ -13,13 +22,40 @@ public class HashMap {
         this.verbosity = verbosity;
     }
 
+    /**
+     * Default constructor
+     */
     public HashMap(){
         this(0);
     }
 
+    /**
+     * Simple hash entry getter.
+     * 
+     * @param key - Key for the required entry.
+     * @return The required entry.
+     */
+    public HashEntry get(String key){
+        int index = this.hash(key);
+        return array[index];
+    }
+
+    /** 
+     * Insert new element into HashMap. Handles collisions through chaining.
+     * Does not support non-string keys or values.
+     * 
+     * @param key - Key to be hashed with.
+     * @param val - Value to be stored.
+     */
     public void insert(String key, String val){
         int index = this.hash(key);
-        this.array[index] = new HashEntry(key, val);
+
+        // Check for collisions
+        if (this.array[index] != null){
+            this.handleCollision(index, new HashEntry(key, val));
+        } else {
+            this.array[index] = new HashEntry(key, val);
+        }
         this.length += 1;
 
         // Increase size if more than half of array is filled. Resize before next insertion for efficiency.
@@ -30,11 +66,6 @@ public class HashMap {
         if (verbosity == 1){
             System.out.printf("Capacity: %d, Length: %d", this.capacity, this.length);
         }
-    }
-
-    public HashEntry get(String key){
-        int index = this.hash(key);
-        return array[index];
     }
 
     /**
@@ -51,6 +82,23 @@ public class HashMap {
         return asciiSum % this.capacity;
     }
 
+    /**
+     * Handle collision through chaining hash entries.
+     * 
+     * @param index - Where collision happens in backing array.
+     * @param newEntry - Entry to be inserted in chain.
+     */
+    private void handleCollision(int index, HashEntry newEntry){
+        HashEntry entry = this.array[index];
+        while (entry.chainedEntry != null) {
+            entry = entry.chainedEntry;  // Iterate through the collision chain
+        }
+        entry.chainedEntry = newEntry;
+    }
+
+    /**
+     * Double capacity & rehash all existing entries. Best called after insertion (before next insert).
+     */
     private void resize(){
         this.capacity = this.capacity * 2;
 
@@ -60,9 +108,12 @@ public class HashMap {
         for (HashEntry entry : oldArray){
             if (entry != null){
                 int index = this.hash(entry.key);
-                this.array[index] = entry;
+                if (this.array[index] != null){
+                    this.handleCollision(index, entry);
+                } else {
+                    this.array[index] = entry;
+                }
             }
         }
     }
-
 }
